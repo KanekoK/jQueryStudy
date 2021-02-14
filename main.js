6a1e68a34722cb2bc9b2b636cdaecbea58228455
kaneko <b1r2d3q4@gmail.com>
2021-02-14 15:54:38 +0900

    演算、条件

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-$('.sub_title p').text('テスト');
-$('.sub_title p').css('color', 'red');
+$subTitle = $('#sub-title p'); // 変数
+
+// $subTitle.text('テスト');
+$subTitle.css('color', 'red');
+
+// 演算
+// $subTitle.text(10 + 10);
+// $subTitle.text('test' + 'foo');
+
+// if文、for文
+console.log($subTitle.text());
+
+if ($subTitle.text() < 10) {
+  $subTitle.text('10未満です！');
+} else {
+  $subTitle.text('10以上です！');
+}
+// if ($subTitle.text())
